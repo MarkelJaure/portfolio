d964fd66ce5137ac4b01b79dae1f11393ec8ba66
Markel <markeljaureguibehere@hotmail.com.ar>
2025-02-21 17:45:03 -0300

    carrusel de imagenes de proyectos, proyecto ANP

--- src/components/proyects.tsx
+++ src/components/proyects.tsx
@@ -18,6 +18,56 @@ import useTechnologies from '@/hooks/useTecnologies';
 import ToolTag from './ToolTag/ToolTag';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import { Key, useEffect, useState } from 'react';
+
+const ImageSlider = ({ images }: any) => {
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const theme = useTheme();
+	const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentIndex((prevIndex: number) => (prevIndex + 1) % images.length);
+		}, 5000);
+		return () => clearInterval(interval);
+	}, [images.length]);
+
+	return (
+		<Box
+			sx={{
+				position: 'relative',
+				width: '100%',
+				height: '100%',
+				overflow: 'hidden',
+				transition: 'transform 0.3s ease-in-out',
+				'&:hover': {
+					transform: 'scale(1.05)', // Aplica a todas las imágenes
+				},
+			}}
+		>
+			{images.map(
+				(image: string | undefined, index: Key | null | undefined) => (
+					<CardMedia
+						key={index}
+						component='img'
+						src={image}
+						sx={{
+							position: 'absolute',
+							top: 0,
+							left: 0,
+							width: '100%',
+							height: '100%',
+							objectFit: 'cover',
+							transition: 'opacity 1s ease-in-out',
+							opacity: index === currentIndex ? 1 : 0,
+						}}
+						alt='Project Image'
+					/>
+				)
+			)}
+		</Box>
+	);
+};
 
 const ProyectsSection = () => {
 	const theme = useTheme();
@@ -33,27 +83,27 @@ const ProyectsSection = () => {
 					<strong>turismo</strong> en un solo lugar. Facilita el acceso a
 					eventos y actividades <strong>culturales</strong> de tu ciudad,
 					promoviendo la participación y el consumo <strong>cultural</strong>{' '}
-					local.
+					local. Disponible en la web y en <strong>Google Play</strong>.
 				</>
 			),
-			image: '/mic.png',
+			images: ['/mic.png'],
 			tools: useTechnologies(['Next.js', 'Nest.js', 'PostgreSQL']),
 			previewUrl: 'https://mapainteractivocultural.ar/',
 		},
 		{
-			title: 'Análisis de Tópicos Dinámicos en Noticias de Chubut',
+			title: 'Dynamic Topic Modeling en noticias de Chubut',
 			description: (
 				<>
-					Proyecto de Tesina de grado en{' '}
+					Proyecto de tesina de grado en{' '}
 					<strong>Licenciatura en Informática</strong>. Sistema de análisis de
 					noticias que identifica los temas predominantes en los medios de{' '}
 					<strong>Chubut</strong> y su evolución a lo largo del tiempo. Utiliza{' '}
 					<strong>Machine Learning (ML)</strong> y técnicas avanzadas de{' '}
 					<em>procesamiento de lenguaje natural</em> para el{' '}
-					<strong>modelado de tópicos</strong>.{' '}
+					<strong>modelado de tópicos</strong>.
 				</>
 			),
-			image: '/tesina.png',
+			images: ['/tesina.png'],
 			tools: useTechnologies([
 				'Python',
 				'Plotly',
@@ -63,32 +113,46 @@ const ProyectsSection = () => {
 			paperUrl: 'tesina.pdf',
 		},
 		{
+			title: 'Sistema de encuestas dinámicas para la Península Valdés',
+			description: (
+				<>
+					Plataforma web diseñada para recopilar la experiencia de los
+					visitantes en Península Valdés. El{' '}
+					<strong>sistema de encuestas dinámico</strong> se adapta en tiempo
+					real a las respuestas de los usuarios. Cuenta con internacionalización
+					y visualización de indicadores estadísticos.
+				</>
+			),
+			images: ['/anp.jpg', 'indicadores.jpeg'],
+			tools: useTechnologies(['React', 'Node.js', 'MongoDB']),
+		},
+		{
 			title: 'Vaidika - Yoga en Madryn',
 			description: (
 				<>
-					Vaidika es una pagina de <strong>E-Learning</strong> para la práctica
-					de yoga en Madryn, Cuenta con Backoffice para la carga de cursos /
-					modulos y clases, y una User App con Login para el consumo de cursos.
-					Desplegada en produccion a traves de <strong>Ferozo</strong>
+					Vaidika es una página de <strong>E-Learning</strong> para la práctica
+					de yoga en Madryn. Cuenta con un Backoffice para la carga de cursos,
+					módulos y clases, y una User App con login para el consumo de cursos.
+					Desplegada en producción a través de <strong>Ferozo</strong>.
 				</>
 			),
-			image: '/Vaidika.png',
+			images: ['/Vaidika.png'],
 			tools: useTechnologies(['PHP', 'MySQL']),
 			previewUrl: 'https://www.vaidikayogamadryn.com/',
 		},
 		{
-			title: 'CubeGraph - Practicas de Rubik',
+			title: 'CubeGraph - Prácticas de Rubik',
 			description: (
 				<>
 					<strong>Side project</strong> personal desarrollado como una
 					oportunidad para aprender y mejorar el uso de tecnologías web.
 					CubeGraph es una plataforma interactiva para entusiastas del cubo de{' '}
 					<em>Rubik</em>, donde se puede entrenar con cronómetros de resolución,
-					competiciones en local y analizar su progreso con{' '}
+					competiciones 1vs1 en local y analizar el progreso con{' '}
 					<strong>estadísticas detalladas</strong>.
 				</>
 			),
-			image: '/cubegraph.png',
+			images: ['/cubegraph.png'],
 			tools: useTechnologies(['React', 'JavaScript', 'Firebase']),
 			previewUrl: 'https://cubegraph.firebaseapp.com/practice',
 			sourceUrl: 'https://github.com/MarkelJaure/CubeGraph',
@@ -197,27 +261,9 @@ const ProyectsSection = () => {
 							<Grid item xs={12} md={5}>
 								<Card
 									elevation={3}
-									sx={{
-										height: '100%',
-										display: 'flex',
-										flexDirection: 'column',
-										justifyContent: 'center',
-										overflow: 'hidden',
-									}}
+									sx={{ height: isMobile ? '200px' : '100%' }}
 								>
-									<CardMedia
-										component='img'
-										sx={{
-											height: isMobile ? '200px' : '100%',
-											objectFit: 'cover',
-											transition: 'transform 0.3s ease-in-out',
-											'&:hover': {
-												transform: 'scale(1.05)',
-											},
-										}}
-										image={project.image}
-										alt={project.title}
-									/>
+									<ImageSlider images={project.images} />
 								</Card>
 							</Grid>
 						</Grid>
