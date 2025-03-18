fe4fd9ca269a1d7228575d16b29cc917a5491aee
Markel <markeljaureguibehere@hotmail.com.ar>
2025-03-17 23:03:56 -0300

    agregado sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: 'https://www.tusitioweb.com', // Cambia esto por tu URL real
+    siteUrl: 'https://portfolio-markel-jaureguibeheres-projects.vercel.app/', // Cambia esto por tu URL real
     generateRobotsTxt: true, // Genera un archivo robots.txt
     i18n: {
         locales: ['es', 'en'],
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,60 @@
-'use client';
-
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from '../styles/theme';
 import { Analytics } from '@vercel/analytics/next';
 import { I18nProvider } from './I18nProvider';
+
+export const metadata = {
+	title: 'Markel Jaureguibehere - Portfolio',
+	description: 'Desarrollador Fullstack y Licenciado en Informática.',
+	keywords: [
+		'Desarrollador Fullstack',
+		'React',
+		'Next.js',
+		'JavaScript',
+		'TypeScript',
+		'Node.js',
+		'Portfolio',
+		'Desarrollo Web',
+	],
+	robots: 'index, follow',
+	author: 'Markel Jaureguibehere',
+	icons: '/favicon.ico',
+	openGraph: {
+		title: 'Markel Jaureguibehere - Portfolio',
+		description: 'Desarrollador Fullstack y Licenciado en Informática.',
+		url: 'https://portfolio-markel-jaureguibeheres-projects.vercel.app/',
+		siteName: 'Markel Jaureguibehere - Portfolio',
+		images: [
+			{
+				url: '/profile.webp', // Asegurate de subir esta imagen
+				width: 1200,
+				height: 630,
+				alt: 'Markel Jaureguibehere - Portfolio',
+			},
+		],
+		type: 'website',
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'Markel Jaureguibehere - Portfolio',
+		description: 'Desarrollador Fullstack y Licenciado en Informática.',
+		images: ['/profile.webp'],
+	},
+};
 
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	const metadata = {
-		title: 'Markel Jaureguibehere - Portfolio',
-		description: 'Desarrollador Fullstack y Licenciado en Informática.',
-	};
-
 	return (
 		<html lang='es'>
 			<head>
 				<title>{metadata.title}</title>
 				<meta name='description' content={metadata.description} />
-				<meta name='robots' content='index, follow' />
-				<link rel='icon' href='/favicon.ico' />
+				<meta name='robots' content={metadata.robots} />
+				<meta name='author' content={metadata.author} />
+				<link rel='icon' href={metadata.icons} />
 				<link
 					rel='canonical'
 					href='https://portfolio-markel-jaureguibeheres-projects.vercel.app/'
