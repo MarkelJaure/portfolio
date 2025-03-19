b38f0228715c116edb880ba249321a2d36906c0b
Markel <markeljaureguibehere@hotmail.com.ar>
2025-03-19 13:11:35 -0300

    Konami code en footer

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -1,37 +1,89 @@
-import { Typography, Box, Link, Container } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Typography, Box, Container, Link } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { useTranslation } from 'react-i18next';
+
+const konamiCode = [
+	'ArrowUp',
+	'ArrowUp',
+	'ArrowDown',
+	'ArrowDown',
+	'ArrowLeft',
+	'ArrowRight',
+	'ArrowLeft',
+	'ArrowRight',
+];
+let inputSequence: any[] = [];
 
 const data = {
 	name: 'Markel Jaureguibehere',
 	repoUrl: 'https://github.com/MarkelJaure/portfolio',
 };
-const Footer = () => {
-	const { t } = useTranslation();
-	return (
-		<Box component='footer' py={3} bgcolor='background.paper'>
-			<Container maxWidth='sm'>
-				<Typography variant='body2' color='text.secondary' align='center'>
-					© {new Date().getFullYear()} {data.name}
-				</Typography>
 
-				<Link
-					href={data.repoUrl}
-					mt={1}
-					gap={1}
-					target='_blank'
-					rel='noopener noreferrer'
-					color='inherit'
-					display='flex'
-					justifyContent='center'
-					alignItems={'center'}
-					flexDirection={'row'}
+const Footer = () => {
+	const [konamiMode, setKonamiMode] = useState(false);
+	const { t } = useTranslation();
+
+	useEffect(() => {
+		const handleKeyDown = (event: { key: any }) => {
+			inputSequence.push(event.key);
+			console.log('inputSequence', inputSequence);
+			if (inputSequence.toString().includes(konamiCode.toString())) {
+				setKonamiMode(true);
+				inputSequence = [];
+			}
+			if (inputSequence.length > konamiCode.length) inputSequence.shift();
+		};
+
+		window.addEventListener('keydown', handleKeyDown);
+		return () => window.removeEventListener('keydown', handleKeyDown);
+	}, []);
+
+	return (
+		<Box
+			component='footer'
+			py={3}
+			bgcolor={'background.paper'}
+			color={'text.secondary'}
+			textAlign='center'
+			sx={{ transition: '0.3s' }}
+		>
+			<Container maxWidth='sm'>
+				<Typography
+					variant='body2'
+					sx={{
+						fontWeight: konamiMode ? 'bold' : 'normal',
+						fontSize: konamiMode ? '1.2rem' : '1rem',
+					}}
+					color={konamiMode ? 'highlight.primary' : 'text.secondary'}
 				>
-					<GitHubIcon />
+					{konamiMode
+						? 'Hola singletoneta!'
+						: 'Hecho con código y mucho mate 🧉'}
+				</Typography>
+				{konamiMode ? (
 					<Typography variant='body2' color='text.secondary'>
-						{t('footer.source')}
+						{'Ustedes no me roben el codigo'}
 					</Typography>
-				</Link>
+				) : (
+					<Link
+						href={data.repoUrl}
+						mt={1}
+						gap={1}
+						target='_blank'
+						rel='noopener noreferrer'
+						color='inherit'
+						display='flex'
+						justifyContent='center'
+						alignItems={'center'}
+						flexDirection={'row'}
+					>
+						<GitHubIcon />
+						<Typography variant='body2' color='text.secondary'>
+							{t('footer.source')}
+						</Typography>
+					</Link>
+				)}
 			</Container>
 		</Box>
 	);
